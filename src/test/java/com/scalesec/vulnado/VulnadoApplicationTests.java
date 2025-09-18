e194b9a56cc09f677b776720ae7cb57278773e6e
wynxx1892 <wynxx1892@gmail.com>
2025-09-18 12:15:51 +0200

    [GFTCodeFixer]: Update on src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java

--- src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class VulnadoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+    throw new UnsupportedOperationException("Method not implemented");
 	}
 
 }
