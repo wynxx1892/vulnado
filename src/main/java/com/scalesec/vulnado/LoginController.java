ac84d87b8311cab05994146e506e5e7a40321f62
wynxx1892 <wynxx1892@gmail.com>
2025-09-18 11:56:30 +0200

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LoginController.java

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -1,10 +1,8 @@
 package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.beans.factory.annotation.*;
 import java.io.Serializable;
 
@@ -14,8 +12,8 @@ public class LoginController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @CrossOrigin(origins = "*") // Ensure enabling CORS is safe and restrict origins in production
+  @PostMapping(value = "/login", produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
     User user = User.fetch(input.username);
     if (Postgres.md5(input.password).equals(user.hashedPassword)) {
@@ -27,15 +25,24 @@ public class LoginController {
 }
 
 class LoginRequest implements Serializable {
-  public String username;
-  public String password;
+  private String username;
+public String getUsername() {
+  private String password;
+    return username;
+}
 }
 
+public String getPassword() {
 class LoginResponse implements Serializable {
-  public String token;
-  public LoginResponse(String msg) { this.token = msg; }
+    return password;
+  private String token;
 }
+  public LoginResponse(String msg) { this.token = msg; }
+public String getToken() {
+}
+    return token;
 
+}
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 class Unauthorized extends RuntimeException {
   public Unauthorized(String exception) {
