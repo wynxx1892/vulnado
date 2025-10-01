e31c9cc412952ca2ad661ed393b359d8a5ece360
wynxx1892 <wynxx1892@gmail.com>
2025-10-01 16:31:33 +0200

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -3,15 +3,17 @@ package com.scalesec.vulnado;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.logging.Logger;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
 
 public class User {
-  public String id, username, hashedPassword;
+  private String id;
+private String username;
 
+private String hashedPassword;
   public User(String id, String username, String hashedPassword) {
     this.id = id;
     this.username = username;
@@ -20,7 +22,7 @@ public class User {
 
   public String token(String secret) {
     SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
-    String jws = Jwts.builder().setSubject(this.username).signWith(key).compact();
+    return Jwts.builder().setSubject(this.username).signWith(key).compact();
     return jws;
   }
 
@@ -31,7 +33,6 @@ public class User {
         .setSigningKey(key)
         .parseClaimsJws(token);
     } catch(Exception e) {
-      e.printStackTrace();
       throw new Unauthorized(e.getMessage());
     }
   }
@@ -42,23 +43,23 @@ public class User {
     try {
       Connection cxn = Postgres.connection();
       stmt = cxn.createStatement();
-      System.out.println("Opened database successfully");
+      Logger logger = Logger.getLogger(User.class.getName());
+logger.info("Opened database successfully");
 
       String query = "select * from users where username = '" + un + "' limit 1";
-      System.out.println(query);
-      ResultSet rs = stmt.executeQuery(query);
+      logger.info(query);
+      PreparedStatement pstmt = cxn.prepareStatement("SELECT * FROM users WHERE username = ? LIMIT 1");
+pstmt.setString(1, un);
       if (rs.next()) {
-        String user_id = rs.getString("user_id");
+        String userId = rs.getString("userid");
         String username = rs.getString("username");
         String password = rs.getString("password");
         user = new User(user_id, username, password);
       }
       cxn.close();
     } catch (Exception e) {
-      e.printStackTrace();
-      System.err.println(e.getClass().getName()+": "+e.getMessage());
+      logger.severe(e.getClass().getName() + ": " + e.getMessage());
     } finally {
-      return user;
     }
   }
 }
